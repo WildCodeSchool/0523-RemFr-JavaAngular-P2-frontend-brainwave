caee4f172ec0dfbbafd4ce3c78c1156d1dd35c35
Enzo Regnier <regnier.enzopro@gmail.com>
2023-07-31 02:10:15 +0200

    Promotion GetAll for Home

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,33 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Promotion } from 'src/models/Promotion';
+import { PromotionsService } from 'src/app/services/promotions.services';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss'],
 })
-export class AccueilComponent {
-  allSquares = [
-    { title: 'Titre Carré 1', content: 'Contenu Carré 1' },
-    { title: 'Titre Carré 2', content: 'Contenu Carré 2' },
-    { title: 'Titre Carré 3', content: 'Contenu Carré 3' },
-    { title: 'Titre Carré 4', content: 'Contenu Carré 4' },
-    { title: 'Titre Carré 5', content: 'Contenu Carré 5' },
-    { title: 'Titre Carré 6', content: 'Contenu Carré 6' },
-  ];
+export class AccueilComponent implements OnInit {
+  filteredPromotions: Promotion[] = [];
+  allPromotions: Promotion[] = [];
 
-  filteredSquares: { title: string; content: string }[];
+  constructor(private promotionsService: PromotionsService) {}
 
-  constructor() {
-    this.filteredSquares = [...this.allSquares];
+  ngOnInit(): void {
+    this.promotionsService.getPromotions().subscribe(
+      (response: any) => {
+        console.log(response);
+        this.allPromotions = response as Promotion[];
+        this.filteredPromotions = [...this.allPromotions];
+      },
+      (error) => {
+        console.error('Failed to fetch promotions', error);
+      }
+    );
   }
 
-  filterSquares(event: Event): void {
+  filterPromotions(event: Event): void {
     const searchTerm = (event.target as HTMLInputElement).value;
     if (searchTerm.trim() === '') {
-      this.filteredSquares = [...this.allSquares];
+      this.filteredPromotions = [...this.allPromotions];
     } else {
-      this.filteredSquares = this.allSquares.filter((square) =>
-        square.title.toLowerCase().includes(searchTerm.toLowerCase())
+      this.filteredPromotions = this.allPromotions.filter((promo) =>
+        promo.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
   }
