f34c002dd0b38a78fbce409c96ee28a02dcf8610
HeleneB1 <helene.blanche@hotmail.fr>
2023-08-03 00:09:19 +0200

    fix bugs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isMenuOpen = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authservice : AuthService) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.closeMenu();
       }
     });
   }
-
+  ngOnInit(): void {
+  
+  }
+  logout() {
+    this.authservice.logout();
+    alert('Vous venez de vous dèconnecter avec succes')
+    this.router.navigate(['/'])
+  }
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -9,29 +9,28 @@ export class StarRatingComponent implements OnInit {
   @Input() totalStars = 5;
   @Input() rating = 3.5;
 
-  stars: number[] = [];
-
+  stars: number[] = [1, 2, 3, 4, 5];
   isVoteModified: boolean = false;
 
   @Output() ratingChanged: EventEmitter<number> = new EventEmitter<number>();
 
   ngOnInit() {
-   
     this.stars = Array.from({ length: this.totalStars }, (_, index) => index + 1);
+    this.rating = Math.min(Math.max(this.rating, 1), this.totalStars); 
+    this.isVoteModified = true; 
   }
-
+    
   rate(star: number) {
     this.rating = star;
     this.isVoteModified = true;
     this.ratingChanged.emit(this.rating);
+    
   }
-
   isStarFilled(star: number): boolean {
     return star <= this.rating;
   }
-
-  isHalfStarDisplayed(star: number): boolean {
-    const floorValue = Math.floor(this.rating);
-    return star === floorValue + 0.5 && this.rating % 1 !== 0;
+    isHalfStarDisplayed(star: number): boolean {
+    const floorValue = Math.floor(this.totalStars);
+    return star === floorValue + 0.5 && this.totalStars % 1 !== 0;
   }
-}
+  }
